5c6c90239eddcf71d7b63f5b632ae204c279f198
福狼 <bsyz_Phlex@hotmail.com>
2025-12-19 12:12:53 +0800

    新增IAccountCartService接口上层封装

---
+++ pkg/steam/api/dev/IAccountCartService.go
@@ -0,0 +1,116 @@
+// Package game 提供 Steam 游戏信息相关 API 封装
+// 核心能力包括已拥有游戏查询、游戏详情获取等, 支持免费游戏筛选、多平台时长统计
+// Package game provides API encapsulation for Steam game information
+// Core capabilities include owned games query, game details retrieval, supports free game filtering and multi-platform playtime statistics
+
+package dev
+
+import (
+	"fmt"
+	"net/url"
+
+	"github.com/GoFurry/gf-steam-sdk/pkg/models"
+	"github.com/GoFurry/gf-steam-sdk/pkg/util"
+	"github.com/GoFurry/gf-steam-sdk/pkg/util/errors"
+	"github.com/bytedance/sonic"
+)
+
+const (
+	IAccountCartService = util.STEAM_API_BASE_URL + "IAccountCartService"
+)
+
+// ============================ 原始字节流接口 ============================
+
+// GetUserCartRawBytes requires access_token, return cart info from the access_token's owner.
+// If globally init access_token, you can use access_token = nil.
+// 返回 access_token 拥有者的购物车信息, 若 access_token 全局初始化则可填 nil.
+func (s *DevService) GetUserCartRawBytes(countryCode string, accessToken *string) (respBytes []byte, err error) {
+
+	params := url.Values{}
+	params.Set("user_country", countryCode)
+	if accessToken != nil {
+		params.Set("access_token", *accessToken)
+	}
+
+	resp, err := s.client.DoRequest("GET", IAccountCartService+"/GetCart/v1/", params)
+	if err != nil {
+		return respBytes, err
+	}
+
+	respBytes, err = sonic.Marshal(resp)
+	if err != nil {
+		return respBytes, fmt.Errorf("%w: marshal resp failed: %v", errors.ErrAPIResponse, err)
+	}
+
+	return respBytes, nil
+}
+
+// ============================ 结构化原始模型接口 ============================
+
+// GetUserCartRawModel requires access_token, return cart info from the access_token's owner.
+// If globally init access_token, you can use access_token = nil.
+// 返回 access_token 拥有者的购物车信息, 若 access_token 全局初始化则可填 nil.
+func (s *DevService) GetUserCartRawModel(countryCode string, accessToken *string) (models.SteamUserCartResponse, error) {
+	bytes, err := s.GetUserCartRawBytes(countryCode, accessToken)
+	if err != nil {
+		return models.SteamUserCartResponse{}, err
+	}
+
+	var cartResp models.SteamUserCartResponse
+	if err = sonic.Unmarshal(bytes, &cartResp); err != nil {
+		return models.SteamUserCartResponse{}, fmt.Errorf("%w: unmarshal cart resp failed: %v", errors.ErrAPIResponse, err)
+	}
+
+	return cartResp, nil
+}
+
+// ============================ 精简模型接口 ============================
+
+// GetUserCartBrief requires access_token, return cart info from the access_token's owner.
+// If globally init access_token, you can use access_token = nil.
+// 返回 access_token 拥有者的购物车信息, 若 access_token 全局初始化则可填 nil.
+func (s *DevService) GetUserCartBrief(countryCode string, accessToken *string) (models.UserCart, error) {
+	rawCart, err := s.GetUserCartRawModel(countryCode, accessToken)
+	if err != nil {
+		return models.UserCart{}, err
+	}
+
+	items := make([]models.CartItems, 0, len(rawCart.Response.Cart.LineItems))
+	for _, i := range rawCart.Response.Cart.LineItems {
+		item := models.CartItems{
+			LineItemID:     i.LineItemID,
+			PackageID:      i.PackageID,
+			Price:          i.PriceWhenAdded.AmountInCents,
+			FormattedPrice: i.PriceWhenAdded.FormattedAmount,
+			AddTime:        util.TimeUnix2String(i.TimeAdded),
+			IsGift:         i.Flags.IsGift,
+			IsPrivate:      i.Flags.IsPrivate,
+		}
+		items = append(items, item)
+	}
+
+	return models.UserCart{
+		Items:          items,
+		TotalPrice:     rawCart.Response.Cart.Subtotal.AmountInCents,
+		FormattedTotal: rawCart.Response.Cart.Subtotal.FormattedAmount,
+	}, nil
+}
+
+// GetUserCart requires access_token, return cart info from the access_token's owner.
+// If globally init access_token, you can use access_token = nil.
+// 返回 access_token 拥有者的购物车信息, 若 access_token 全局初始化则可填 nil.
+func (s *DevService) GetUserCart(countryCode string, accessToken *string) (models.UserCart, error) {
+	return s.GetUserCartBrief(countryCode, accessToken)
+}
+
+// DeleteUserCart requires access_token, clear all cart items from the access_token's owner.
+// If globally init access_token, you can use access_token = nil.
+// 清空 access_token 拥有者的购物车, 若 access_token 全局初始化则可填 nil.
+func (s *DevService) DeleteUserCart(accessToken *string) error {
+	params := url.Values{}
+	if accessToken != nil {
+		params.Set("access_token", *accessToken)
+	}
+	_, err := s.client.DoRequest("POST", IAccountCartService+"/DeleteCart/v1/", params)
+	return err
+}
